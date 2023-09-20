958cf3245bbabd4967852f00c6000a597afc8719
Vlad Glazunov <vlad.glazunov.02@gmail.com>
2023-09-21 01:26:45 +0300

    feat: add JwtTokenUtils for jwt usage

---
+++ src/main/java/com/justtown/channel_service/util/JwtTokenUtils.java
@@ -0,0 +1,31 @@
+package com.justtown.channel_service.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenUtils {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.lifetime}")
+    private long lifetime;
+
+    public String getUsername(String token) {
+        return getAllClaimsFromToken(token).getSubject();
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+}
